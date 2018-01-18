21a5d9c196fca444078ef33076e75e92410e708c
Ali Schlereth <aschlere@sbcglobal.net>
2018-01-18 13:59:51 -0700

    Add comment partial

--- spec/features/user_sees_one_article_spec.rb
+++ spec/features/user_sees_one_article_spec.rb
@@ -4,8 +4,8 @@ describe "user sees one article" do
   describe "they link from the article index" do
     it "displays information for one article" do
       article = Article.create!(title: "New Title", body: "New Body")
-      comment_1 = article.comments.new(author_name: "Me", body: "Commenty comments")
-      comment_2 = article.comments.new(author_name: "You", body: "So much to say")
+      comment_1 = article.comments.create(author_name: "Me", body: "Commenty comments")
+      comment_2 = article.comments.create(author_name: "You", body: "So much to say")
 
       visit articles_path
 
