1b2dbe337db2c8f5b9eb71b3866130bccc85d5e7
Ali Schlereth <aschlere@sbcglobal.net>
2018-01-18 12:57:08 -0700

    Add flash for create and destroy with tests

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+    flash.notice = "Article #{@article.title} Created!"
     redirect_to article_path(@article)
   end
 
@@ -29,6 +30,7 @@ class ArticlesController < ApplicationController
 
   def destroy
     @article.destroy
+    flash.notice = "Article #{@article.title} Deleted!"
     redirect_to articles_path
   end
 
